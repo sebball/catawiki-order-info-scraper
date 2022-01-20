d560c356a382a77e78a385c4ef5724e741e0db7b
sebball <sebby00@gmail.com>
2022-01-20 03:03:46 +0100

    Catawiki order information scraper

---
+++ cwCtrls.py
@@ -0,0 +1,76 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import logging
+import re
+import time
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
+
+
+
+def agreeCookies(webDriver):
+    # select element of cookie agree button and press it
+    cookieAgreeElem = webDriver.find_element_by_id('cookie_bar_agree_button')
+
+    cookieAgreeElem.click()
+
+
+def signIn(webDriver, accountInfo):
+    """Sign in to catawiki. Takes the arguments webDriver, and accountInfo. accountInfo is a list with index 0 being
+    the desired username and index 1 being the password"""
+    # Set password depending on username
+    username = accountInfo[0]
+    password = accountInfo[1]
+
+    # find element of sign in button and click it going to sign in page
+    signinElem = webDriver.find_element_by_id('header-sign-in')
+    signinElem.click()
+
+    # find element of username field and fill in username
+    usernameElem = webDriver.find_element_by_id('user_name')
+    usernameElem.send_keys(username)
+
+    # find element of password field and fill in password
+    passwordElem = webDriver.find_element_by_id('password')
+    passwordElem.send_keys(password)
+
+    # click sign in button
+    signinElem = webDriver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div/form/input[3]')
+    signinElem.click()
+
+
+def goToOfferedLots(webDriver, page=1):
+    if page == 1:
+        webDriver.get('https://www.catawiki.com/en/seller/offered-lots')
+    elif page > 1:
+        webDriver.get(f'https://www.catawiki.com/en/seller/offered-lots/?page={page}')
+
+
+def goToSoldLots(webDriver, page=1):
+    if page == 1:
+        webDriver.get('https://www.catawiki.com/en/fulfilment/seller/orders?archived=false&page=1&per_page=25')
+    if page > 0:
+        webDriver.get(f'https://www.catawiki.com/en/fulfilment/seller/orders?archived=false&page={page}&per_page=25')
+
+
+
+def addNumsTitles(webDriver, info_dictionary):
+    """Intended for use when on a 'Sold Lots' page. Passed a dictionary the function will add a new dictionary for each
+     order on the page. The parent key of this dictionary will be the order number and the dictionary will contain one
+     key 'Title text' which contains the title of the catawiki lot."""
+    lotElems = webDriver.find_elements(By.CLASS_NAME, 'fu-lot-link')
+    country_elems = webDriver.find_elements(By.XPATH, '//tr[@class="fu-seller-order-list-table__row"]/td[6]/div[2]')
+    for i, elem in enumerate(lotElems):
+        # Links to the order page are in the href attribute. Order numbers need to be extracted from this string.
+        elemOrderLink = (elem.get_attribute('href'))
+        elemOrderNum = int(''.join(filter(str.isdigit, elemOrderLink)))
+
+        # This is the title of each lot corresponding to the order number
+        elemTitleText = elem.text
+
+        #This is the country ordered from
+        country = country_elems[i].text
+
+        # Add order number as key to the order information dictionary, and the other information as keys to that dictionary
+        info_dictionary[elemOrderNum] = {"Title text": elemTitleText, "Country": country}
+
---
+++ main.py
@@ -0,0 +1,109 @@
+import cwCtrls
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import logging
+import re
+import time
+
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
+
+# Set up browser and log into catawiki
+targetValue = 24776079
+driver = webdriver.Firefox()
+driver.get('https://catawiki.com')
+cwCtrls.agreeCookies(webDriver=driver)
+cwCtrls.signIn(webDriver=driver, username='eva@sooka.com')
+
+# Create empty dictionary  which will contain all further order information. The dictionary will be filled with
+# dictionaries of information about each order with the order number acting as the key for each dictionary
+orderInfoDict = {}
+
+# Starting at page 1 the while loop will go to the sold lots page and extract the order numbers from the page. These
+# will be stored as keys in order_info_dict with the values being new dictionaries containing the title text and the
+# country.  After all orders have been extracted from the loop the lowest ordder number will be assigned to lowestNumber
+# and if this is lower than targetValue the loop will stop. Other wise it will continue to the next page.
+lowestNumber = 9999999999
+pageNumber = 1
+while lowestNumber > targetValue:
+    cwCtrls.goToSoldLots(webDriver=driver, page=pageNumber)
+    time.sleep(5)
+    #Add the order numbers and lot titles as series of nested dictionaries ie {ordernum:{'title': actualtitle}}
+    cwCtrls.addNumsTitles(webDriver=driver, info_dictionary=orderInfoDict)
+    # Assign the lowest order number value to LowestNumber so loop stops if needed
+    lowestNumber = min(orderInfoDict.keys())
+    pageNumber += 1
+
+# There are likely to be a number of order numbers lower than the target orderInfoDict. Now we loop through the keys
+# and delete those with a lower value in order to make sure we only have the desired orders in the dictionary
+keys_to_check = list(orderInfoDict)
+for order_num in keys_to_check:
+    print(len(orderInfoDict))
+    if order_num < targetValue:
+        del orderInfoDict[order_num]
+print(len(orderInfoDict), orderInfoDict)
+
+# Loop through the order numbers in orderInfoDict and add order information to the corresponding dictionary
+orderNumbers = list(orderInfoDict.keys())
+for orderNumber in orderNumbers:
+    driver.get(f"https://www.catawiki.com/en/fulfilment/seller/orders/{orderNumber}")
+    time.sleep(3)
+    # Order Status
+    orderInfoDict[orderNumber]['Order Status'] = driver.find_element(by=By.XPATH, value='//p[@data-testid="order-'
+                                                                                        'details-status"]').text
+    # Date paid
+    orderInfoDict[orderNumber]["Date Paid"] = driver.find_element(by=By.XPATH, value='//td[@class="u-p-r'
+                                                                                     '-xs u-no-wrap"]').text
+    # Shipment Date
+    try:
+        orderInfoDict[orderNumber]["Date Shipped"] = driver.find_element(by=By.XPATH, value='//table[@data-testid="orde'
+                                                                                            'r-events"]/tbody/tr[2]/td'
+                                                                                            '[1]').text
+    except NoSuchElementException:
+        orderInfoDict[orderNumber]["Date Shipped"] = "Not yet shipped"
+
+    # Reference Tag
+    orderInfoDict[orderNumber]["Reference Tag"] = driver.find_element(by=By.XPATH, value='//div[@class="fu-lot-label"]/'
+                                                                                         'div[@class="u-font-size-xs-ti'
+                                                                                         'ght u-color-secondary u-word-'
+                                                                                         'break-all"]').text
+    # Item Price
+    orderInfoDict[orderNumber]["Item Price"] = driver.find_element(by=By.XPATH, value='//dd[@data-testid="order-summary'
+                                                                                      '-items"]').text
+
+    # Shipping Price
+    orderInfoDict[orderNumber]["Shipping Price"] = driver.find_element(by=By.XPATH, value='//dd[@data-testid="order-su'
+                                                                                          'mmary-items"]').text
+
+    # Go to auction page
+    auction_link = driver.find_element(by=By.XPATH, value='//a[@class="c-link main"]').get_attribute('href')
+    driver.get(auction_link)
+    time.sleep(1)
+
+    # Auction name
+    auction_name = driver.find_element(by=By.XPATH, value='//a[@class="c-breadcrumbs__item c-breadcrumbs__item'
+                                                          '--last"]').text
+    orderInfoDict[orderNumber]["Auction Name"] = auction_name
+
+    # Auction end date
+    auction_end = driver.find_element(by=By.XPATH, value='//th[@class="u-color-secondary be-bid-history__unit-col '
+                                                         'be-bid-history__time"]/span[1]')
+    orderInfoDict[orderNumber]["Auction End Date"] = auction_end.text
+
+    print(orderInfoDict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
